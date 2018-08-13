eafa1886c0d80dd05ea32a137f7a2cfce254ae9f
Tristan Brown <tgbrown@stanford.edu>
2018-08-13 23:00:39 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+if speed > 60 || speed < 40
+	true
+else
+	false
+end
 end
 
 
 
 def not_safe?(speed)
-	
+speed > 60 || speed < 40? true : false
+speed >= 40 && speed <= 60 ? false : true
 end
-	
-
-
