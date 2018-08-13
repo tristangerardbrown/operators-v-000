22e40388c94c1d02386a85a78e525e8991812db9
Tristan Brown <tgbrown@stanford.edu>
2018-08-13 22:58:00 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -4,14 +4,15 @@ if speed > 60
 else
 	false
 end
-end 
+end
 
 
 
 def not_safe?(speed)
 speed > 60? true : false
-speed < 40? true : false 
-speed >= 40 && speed <= 60 ? false : true 
 end 
+speed < 40? true : false
 end 
+speed >= 40 && speed <= 60 ? false : true
+end
 
